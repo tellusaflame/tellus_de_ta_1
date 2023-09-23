3343b12d63021f8b11ec5b300398886c1202ce5a
tellusaflame <tellus.aflame@outlook.com>
2023-09-23 21:11:47 +0300

    CHANGE: "INSERT INTO" w/o cycle CHANGE: psycopg2 cursor and connection into function ADDED: .env with pg creds ADDED: __main__ and main() REMOVED: Removed # with code CHANGE: "print" replaced with "logging" UPDATE: requirements.txt

--- main.py
+++ main.py
@@ -3,9 +3,25 @@ from bs4 import BeautifulSoup
 
 import re
 import copy
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+postgres_secrets = {
+    "host": os.getenv("POSTGRES_HOST"),
+    "port": os.getenv("POSTGRES_PORT"),
+    "user": os.getenv("POSTGRES_USER"),
+    "password": os.getenv("POSTGRES_PASSWORD"),
+    "database": os.getenv("POSTGRES_DB"),
+}
 
 import psycopg2
 from psycopg2 import Error
+from psycopg2.extras import execute_batch
+
+import logging
 
 def scrape_wikipedia(city_names):
     cities_data = []
@@ -25,20 +41,16 @@ def scrape_wikipedia(city_names):
             "Почтовый индекс": ""
         }
 
-        # infobox = soup.find("table", class_="infobox geography vcard")
-        # infobox = soup.find("table", class_="infobox ib-settlement vcard")
         infobox = soup.find("table", class_="infobox")
-        #print(infobox)
 
         if infobox is not None:
             rows = infobox.find_all("tr")
 
             for row in rows:
                 header = row.find("th")
-                #print(header)
+
                 if header is not None:
                     header_text = header.text.strip()
-                    #print(header_text)
 
                     if header_text == "Страна":
                         city_data["Страна"] = row.find("td").text.strip()
@@ -56,7 +68,7 @@ def scrape_wikipedia(city_names):
                         try:
                             city_data["Население"] = row.find("td").text.strip()
                         except:
-                            print("pizdec")
+                            logging.info("pizdec")
 
                     elif header_text in ["Почтовые индексы", "Почтовый индекс"]:
                         city_data["Почтовый индекс"] = row.find("td").text.strip()
@@ -87,72 +99,58 @@ def clean_data(raw_data):
 
     return cleaned_cities_data
 
-# List of cities to obtain data from
-city_names = ["Москва", "Тимашёвск", "Иркутск", "Калуга", "Владивосток"]
+def create_cursor(**secrets):
+    connection = psycopg2.connect(user=secrets['user'],
+                                  password=secrets['password'],
+                                  host=secrets['host'],
+                                  port=secrets['port'],
+                                  database=secrets['database'])
 
-# Get raw data from scrapper
-raw_data = scrape_wikipedia(city_names)
-
-# Clean received data
-cleaned_data = clean_data(raw_data)
-
-# for city_data in raw_data:
-#     print(city_data)
-#     print()
-
-for city_data in cleaned_data:
-    print(city_data)
-    print()
-
-# Open connection to db
-try:
-    # Connect to desired DB
-    connection = psycopg2.connect(user="user",
-                                  password="password",
-                                  host="127.0.0.1",
-                                  port="5432",
-                                  database="database")
     # Open a cursor to perform database operations
     cursor = connection.cursor()
+    return cursor, connection
 
-    # Truncate table city_data
-    cursor.execute(""" TRUNCATE city_data """)
-    connection.commit()
+def main():
+    # List of cities to obtain data from
+    city_names = ["Москва", "Тимашёвск", "Иркутск", "Калуга", "Владивосток"]
 
-    # Cycle all cities, insert data to table
-    for city_data in cleaned_data:
+    # Get raw data from scrapper
+    raw_data = scrape_wikipedia(city_names)
+
+    # Clean received data
+    cleaned_data = clean_data(raw_data)
+
+    # Open connection to db
+    try:
+        cursor, connection = create_cursor(**postgres_secrets)
+
+        # Truncate table city_data
+        cursor.execute(""" TRUNCATE city_data """)
+        connection.commit()
 
         # Form an SQL query
-        insert_query = """INSERT INTO city_data (city, country, founded, climate, time_zone, population, postal_code) 
-        VALUES (%s,%s,%s,%s,%s,%s,%s)"""
+        query = "INSERT INTO city_data VALUES (%(Город)s, %(Страна)s, %(Основан)s, %(Климат)s, %(Часовой пояс)s, %(Население)s, %(Почтовый индекс)s)"
 
-        # Form data to insert within SQL query
-        data_to_insert = (city_data["Город"],
-                            city_data["Страна"],
-                            city_data["Основан"],
-                            city_data["Климат"],
-                            city_data["Часовой пояс"],
-                            city_data["Население"],
-                            city_data["Почтовый индекс"],
-                            )
-
-        # Execute a command: this insert new row to a table
-        cursor.execute(insert_query, data_to_insert)
+        # Execute a command: this insert new rows to a table
+        execute_batch(cursor, query, cleaned_data)
 
         # Make the changes to the database persistent
         connection.commit()
-        print("1 запись успешно вставлена")
+        logging.info("Все записи успешно вставлены")
 
-    # Query the database and obtain data as Python objects
-    cursor.execute("SELECT * from city_data")
-    record = cursor.fetchall()
-    print("Результат", record)
+        # Query the database and obtain data as Python objects
+        cursor.execute("SELECT * from city_data")
+        record = cursor.fetchall()
+        logging.info("Результат", record)
 
-except (Exception, Error) as error:
-    print("Ошибка при работе с PostgreSQL", error)
-finally:
-    if connection:
-        # Close communication with the database
-        cursor.close()
-        connection.close()
-        print("Соединение с PostgreSQL закрыто")
+    except (Exception, Error) as error:
+        logging.exception("Ошибка при работе с PostgreSQL", error)
+    finally:
+        if connection:
+            # Close communication with the database
+            cursor.close()
+            connection.close()
+            logging.info("Соединение с PostgreSQL закрыто")
+
+if __name__ == "__main__":
+    main()
