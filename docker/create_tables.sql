1bcf8e02bb49b0a9014b0cd1e414ebd2332ce7c7
tellusaflame <tellus.aflame@outlook.com>
2023-09-23 16:55:02 +0300

    Added .yml and .sql for docker instance

---
+++ docker/create_tables.sql
@@ -0,0 +1,10 @@
+-- Creation of product table
+CREATE TABLE IF NOT EXISTS city_data (
+  city varchar(250) NOT NULL,
+  country varchar(250) NOT NULL,
+  founded INT NOT NULL,
+  climate varchar(250) NOT NULL,
+  time_zone varchar(250) NOT NULL,
+  population INT NOT NULL,
+  postal_code varchar(6) NOT NULL 
+);
